e69f75f4d5d25d6c8e46d9ca0e61a12abe11ab55
Sebastian <sebastian@core10>
2009-09-03 17:32:15 +0200

    polyeder v 0.1 done, working but shape seems wrong, possibly coord problem, needs fixing

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import input
 import geometry
 import sphere
 import output
+import convex_polyeder
 
 numpy.seterr(divide='ignore',invalid='ignore')
 
@@ -16,10 +17,10 @@ numpy.seterr(divide='ignore',invalid='ignore')
 c=input.read_ini('planes_input.ini')
 d=input.ini2dict(c)
 geo = geometry.geometry.from_dict(d)
-
+#print geo._lattice_vectors[0]
 #sph = sphere.sphere.from_dict(geo, d['sphere'])
 
-poly = convex_poly.convex_poly.from_dict(geo, d)
+poly = convex_polyeder.convex_polyeder.from_dict(geo, d)
 
 #print poly._planes_normal
 
@@ -27,6 +28,6 @@ atoms = geo.gen_cuboid_from_body(poly)
 
 #in_out_array = numpy.array([1 for x in atoms == 1])
 
-in_out_array = convex_poly.sorting(atoms)
+in_out_array = poly.atoms_inside(atoms)
 
 output.write_structure_to_file(geo, atoms, in_out_array, 'out.xyz')
