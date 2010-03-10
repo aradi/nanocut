62caca4475625a8db17a9c1c3042c848509c19e3
Florian Uekermann <florian@FlorianLaptop>
2010-03-10 13:42:49 +0100

    Restructured periodicity.py and added comments

--- periodicity.py
+++ periodicity.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy
 
-
 def gcd(a,b,c):
+  '''Return greatest common divisor.'''
   while b:
     a, b = b, a % b
   while c:
@@ -11,75 +11,68 @@ def gcd(a,b,c):
 
 
 class periodicity:
-  
-
-  def __init__(self,geometry,period_type,axis=None,plane=None,plane_thickness=None):
+  '''Class holding information about type of periodicity, and axes.'''  
+  def __init__(self,geometry,period_type,axis=None):
 
     if period_type=="0D":
       self._period_type="0D"
 
-    elif period_type=="1D":
-      self._period_type="1D"
-      
-      axis=numpy.array(axis,dtype='int')
-      axis.shape=(1,3)
-
-      if (axis==numpy.array([[0,0,0]])).all():
-        axis=None
-      self._axis = axis/gcd(axis[0,0],axis[0,1],axis[0,2])
-      self._axis_cart=geometry.coord_transform(axis, "lattice")
-
-    elif period_type=="2D":
-      self._period_type="2D"
-      
-      axis=numpy.array(axis,dtype='int')
-      axis.shape=(2,3)
-
-      if (axis==numpy.array([[0,0,0]])).all():
-        axis=None
-      self._axis = numpy.vstack((axis[1]/gcd(axis[1,0],axis[1,1],axis[1,2]),
-                                 axis[0]/gcd(axis[0,0],axis[0,1],axis[0,2])))
-      self._axis_cart=geometry.coord_transform(axis, "lattice")
-
     else:
-      #TODO: Raise Exeption
-      exit('InternalError:\n'+
-           'No valid period_type given.'
-           +'\nExiting...\n')
+      axis=numpy.array(axis,dtype='int')
+      axis.shape=(-1,3)
+
+      if period_type=="1D":
+        self._period_type="1D"
+
+        if (axis==0).all():
+          raise ValueError, "Value of axis is invalid."
+
+        #shorten axis if possible by division with gcd
+        self._axis = axis/gcd(axis[0,0],axis[0,1],axis[0,2])
+
+      elif period_type=="2D":
+        self._period_type="2D"
+
+        if ((axis[0]==0).all() or axis[1]==0).all():
+          raise ValueError, "Value of axis is invalid."
+
+        #shorten axes if possiple by division with gcd
+        self._axis = numpy.vstack((axis[1]/gcd(axis[1,0],axis[1,1],axis[1,2]),
+                                   axis[0]/gcd(axis[0,0],axis[0,1],axis[0,2])))
+
+      else:
+        raise ValueError, "Value of period_type is invalid."
+
+      self._axis_cart=geometry.coord_transform(self._axis, "lattice")
 
   def get_axis(self,coordsys="lattice"):
+    '''Returns axis'''
     if (self._period_type=="1D" or self._period_type=="2D"):
       if coordsys=="lattice":
         return self._axis
       elif coordsys=="cartesian":
         return self._axis_cart
       else:
-        #TODO: Raise Exeption
-        exit('InternalError:\n'+
-           'No valid coordinate system given.'
-           +'\nExiting...\n')
+        raise ValueError, "Value of coorsys is invalid."
     else:
-      #TODO: Raise Exeption
-      exit('InternalError:\n'+
-           'get_axis() called, but period_type is not 1D or 2D.'
-           +'\nExiting...\n')
-
+      raise ValueError, "get_axis() called, but period_type is not 1D or 2D."
+      
   def period_type_is(self,testtype):
+    '''Returns True if argument matches period_type.'''
     return self._period_type==testtype
 
 
 
   @classmethod
   def from_dict(cls,geometry,d):
-    #Reads periodicity from dict
+    '''Reads periodicity from dict and checks data types'''
     if "periodicity" not in d.keys():
-      
       return cls(geometry,"0D")
+
     else:
       period_type = d["periodicity"].get("period_type","0D")
       
       if period_type=="0D":
-
         return cls(geometry,"0D")
         
         
@@ -105,6 +98,11 @@ class periodicity:
           'Wrong number of elements supplied for axis.'
            +'\nExiting...\n')
         
+        if (axis==0).all():
+          exit('Error:\n'+
+          'Value of axis is invalid.'
+          +'\nExiting...\n')
+
         return cls(geometry,"1D",axis)
 
 
@@ -124,10 +122,15 @@ class periodicity:
            +'\nExiting...\n')
         
         try:
-          axis.shape=(1,6)
+          axis.shape=(2,3)
         except ValueError:
           exit('Error:\n'+
           'Wrong number of elements supplied for axis.'
            +'\nExiting...\n')
         
+        if ((axis[0]==0).all() or axis[1]==0).all():
+          exit('Error:\n'+
+          'Value of axis is invalid.'
+          +'\nExiting...\n')
+
         return cls(geometry,"2D",axis)
